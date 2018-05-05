357be3a9225c4ca75c9afe8cddd5cd873205f306
GilbertoSevilio <gilbertosevilio@gmail.com>
2018-05-05 18:05:28 -0300

    lec 59

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCTxHjggqGOvMA0zdVD2JC8PzHU4QH4rNw',
+      authDomain: 'auth-61d34.firebaseapp.com',
+      databaseURL: 'https://auth-61d34.firebaseio.com',
+      projectId: 'auth-61d34',
+      storageBucket: 'auth-61d34.appspot.com',
+      messagingSenderId: '551382202007'
+    });
+  }
+
 render() {
-  return(
+  return (
     <View>
+    <Header headerText="Authentication" />
       <Text>An App</Text>
     </View>
   );
--- src/components/common/header.js
+++ src/components/common/header.js
@@ -32,4 +32,4 @@ const styles = {
 };
 
 //Make the component avaliable to other parts of the app/
-export default Header;
+export { Header };
