50d2505339ea737c8b01d1ce5d982e6a3717edb9
GilbertoSevilio <gilbertosevilio@gmail.com>
2018-05-06 11:10:35 -0300

    lec 65, same as lec 61

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state = { text: '' };
@@ -9,8 +8,7 @@ class LoginForm extends Component {
     return (
       <Card>
         <CardSection>
-          {/*<TextInput style={{ height: 20, width: 100 }} /> TextInput on the video */}
-          <TextInput
+          <Input
           value={this.state.text}
             onChangeText={text => this.setState({ text })}
           />
